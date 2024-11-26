40fdebde9a0e25ebdfa2070c3a81d4dc84cbd3ac
EllGHT <52287330+eightroutes@users.noreply.github.com>
2024-11-26 10:52:08 +0900

    fix: 그룹 생성시 이전뷰로, 그룹 추가 이미지 삭제 버튼

--- Connected/Core/TabBar/Group/View/AddGroupView.swift
+++ Connected/Core/TabBar/Group/View/AddGroupView.swift
@@ -20,138 +20,158 @@ struct AddGroupView: View {
     @State private var showAlert = false
     @State private var errorMessage = ""
     
+    @Environment(\.presentationMode) var presentationMode
     
     @StateObject private var vm = GroupViewModel()
     
-    
     var body: some View {
         NavigationStack {
-            VStack {
-                HStack {
-                    // 이미지 추가
-                    ZStack {
-                        Rectangle()
-                            .fill(Color.gray.opacity(0.2))
-                            .frame(width: 120, height: 120)
-                            .shadow(radius: 1)
-                        
-                        if let image = groupImage {
-                            Image(uiImage: image)
-                                .resizable()
-                                .scaledToFill()
+            ScrollView {
+                VStack {
+                    HStack {
+                        // 이미지 추가
+                        ZStack {
+                            Rectangle()
+                                .fill(Color.gray.opacity(0.2))
                                 .frame(width: 120, height: 120)
-                                .clipShape(Rectangle())
+                                .shadow(radius: 1)
+                            
+                            if let image = groupImage {
+                                ZStack(alignment: .topTrailing) {
+                                    Image(uiImage: image)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 120, height: 120)
+                                        .clipShape(Rectangle())
+                                    
+                                    VStack {
+                                        Button(action: {
+                                            groupImage = nil
+                                        }) {
+                                            Image(systemName: "xmark.circle.fill")
+                                                .foregroundStyle(.white)
+                                                .background(Color.black.opacity(0.5))
+                                                .clipShape(Circle())
+                                        }
+                                        .padding(2)
+                                        
+                                    }
+                                }
+                                
+                            } else {
+                                
+                                
+                                Button(action: {
+                                    showingImagePicker = true
+                                }) {
+                                    if groupImage == nil {
+                                        Image(systemName: "plus")
+                                            .resizable()
+                                            .frame(width: 30, height: 30)
+                                            .foregroundColor(.white)
+                                    }
+                                }
+                            }
+                            
+                        }//ZStack
+                        .sheet(isPresented: $showingImagePicker) {
+                            ImagePickerView(isPresented: $showingImagePicker, selectedImage: $groupImage, showCropView: $showingCropView)
+                        }
+                        .fullScreenCover(isPresented: $showingCropView) {
+                            if let image = groupImage {
+                                CropViewControllerWrapper(image: image, croppingShape: .default, croppedImage: $groupImage, isPresented: $showingCropView)
+                            }
+                        }
+                        VStack {
+                            HStack{
+                                TextField("지역", text: $locationText)
+                                    .padding()
+                                    .frame(height: 36)
+                                    .background(Color.gray.opacity(0.2))
+                                    .cornerRadius(5)
+                            }
+                            
+                            
+                            HStack{
+                                TextField("모임 이름", text: $groupName)
+                                    .padding()
+                                    .frame(height: 36)
+                                    .background(Color.gray.opacity(0.2))
+                                    .cornerRadius(5)
+                            }
+                            
+                            HStack {
+                                TextField("주제", text: $theme)
+                                    .padding()
+                                    .frame(height: 36)
+                                    .background(Color.gray.opacity(0.2))
+                                    .cornerRadius(5)
+                                
+                            }
+                            
+                            
                             
                         }
                         
+                    }
+                    
+                    
+                    HStack {
                         
-                        Button(action: {
-                            showingImagePicker = true
-                        }) {
-                            if groupImage == nil {
-                                Image(systemName: "plus")
-                                    .resizable()
-                                    .frame(width: 30, height: 30)
-                                    .foregroundColor(.white)
+                        TextEditor(text: $groupDescription)
+                            .frame(height: 350)
+                            .padding()
+                            .background(Color.gray.opacity(0.2))
+                            .cornerRadius(5)
+                        
+                    }
+                    
+                    
+                    
+                    
+                    Spacer()
+                    
+                    // 오류 메시지 표시
+                    if !errorMessage.isEmpty {
+                        Text(errorMessage)
+                            .foregroundColor(.red)
+                            .font(.footnote)
+                            .padding(.bottom, 5)
+                            .onAppear {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                                    errorMessage = ""
+                                }
                             }
-                        }
-                        
-                    }//ZStack
-                    .sheet(isPresented: $showingImagePicker) {
-                        ImagePickerView(isPresented: $showingImagePicker, selectedImage: $groupImage, showCropView: $showingCropView)
-                    }
-                    .fullScreenCover(isPresented: $showingCropView) {
-                        if let image = groupImage {
-                            CropViewControllerWrapper(image: image, croppingShape: .default, croppedImage: $groupImage, isPresented: $showingCropView)
-                        }
-                    }
-                    VStack {
-                        HStack{
-                            TextField("지역", text: $locationText)
-                                .padding()
-                                .frame(height: 36)
-                                .background(Color.gray.opacity(0.2))
-                                .cornerRadius(5)
-                        }
-                        
-                        
-                        HStack{
-                            TextField("모임 이름", text: $groupName)
-                                .padding()
-                                .frame(height: 36)
-                                .background(Color.gray.opacity(0.2))
-                                .cornerRadius(5)
-                        }
-                        
-                        HStack {
-                            TextField("주제", text: $theme)
-                                .padding()
-                                .frame(height: 36)
-                                .background(Color.gray.opacity(0.2))
-                                .cornerRadius(5)
-            
-                        }
-                        
-                        
-                        
                     }
                     
-                }
-                
-                
-                HStack {
+                    Spacer()
                     
-                    TextEditor(text: $groupDescription)
-                        .frame(height: 350)
-                        .padding()
-                        .background(Color.gray.opacity(0.2))
-                        .cornerRadius(5)
+                    Button {
+                        createGroup()
+                    } label: {
+                        Text("모임 만들기")
+                            .frame(width: 250)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(isValidInput() ? Color.brand : Color.secondary)
+                            .cornerRadius(30)
+                    }
+                    .disabled(!isValidInput())
+                    .alert(isPresented: $showAlert) {
+                        Alert(title: Text("모임 생성 완료"), message: Text("모임이 성공적으로 생성되었습니다."), dismissButton: .default(Text("확인")) {
+                            // 현재 뷰 닫기
+                            presentationMode.wrappedValue.dismiss()
+                            
+                        })
+                    }
+                    
+                    
                     
                 }
-                
-                
-                
-                
-                Spacer()
-                
-                // 오류 메시지 표시
-                if !errorMessage.isEmpty {
-                    Text(errorMessage)
-                        .foregroundColor(.red)
-                        .font(.footnote)
-                        .padding(.bottom, 5)
-                        .onAppear {
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                                errorMessage = ""
-                            }
-                        }
-                }
-
-                
-                Button {
-                    createGroup()
-                } label: {
-                    Text("모임 만들기")
-                        .frame(width: 250)
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(isValidInput() ? Color.brand : Color.secondary)
-                        .cornerRadius(30)
-                }
-                .disabled(!isValidInput())
-                .alert(isPresented: $showAlert) {
-                    Alert(title: Text("모임 생성 완료"), message: Text("모임이 성공적으로 생성되었습니다."), dismissButton: .default(Text("확인")) {
-                        // 네비게이션 또는 다른 동작
-                        
-                    })
-                }
-                
-                
-                
+                .padding()
+                .navigationTitle("모임 만들기")
+                .navigationBarTitleDisplayMode(.inline)
             }
-            .padding()
-            .navigationTitle("모임 만들기")
-            .navigationBarTitleDisplayMode(.inline)
         }
     }
     
--- Connected/Core/TabBar/Group/View/GroupCardView.swift
+++ Connected/Core/TabBar/Group/View/GroupCardView.swift
@@ -12,50 +12,52 @@ struct GroupCardView: View {
     let group: Groups
     
     var body: some View {
-        NavigationLink(destination: GroupDetailView(group: group)) {
-            HStack(alignment: .top) {
-                KFImage(URL(string: group.mainImageUrl))
-                    .resizable()
-                    .frame(width: 60, height: 60)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                //                .overlay(
-                //                    RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 0.1)
-                //                )
-                    .shadow(radius: 1)
-                
-                VStack(alignment: .leading, spacing: 5) {
-                    Text(group.name)
-                        .font(.headline)
+        NavigationStack {
+            NavigationLink(destination: GroupDetailView(group: group)) {
+                HStack(alignment: .top) {
+                    KFImage(URL(string: group.mainImageUrl))
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                    //                .overlay(
+                    //                    RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 0.1)
+                    //                )
+                        .shadow(radius: 1)
                     
-                    Text(group.description)
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                    
-                    HStack {
-                        Text("#\(group.theme)")
-                            .font(.caption)
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text(group.name)
+                            .font(.headline)
+                        
+                        Text(group.description)
+                            .font(.subheadline)
                             .foregroundColor(.gray)
                         
-                        
-                        Text(group.location)
-                            .font(.caption)
-                            .foregroundColor(.gray)
-                        
-                        Text("멤버 \(group.memberCounts)")
-                            .font(.caption)
-                            .foregroundColor(.gray)
+                        HStack {
+                            Text("#\(group.theme)")
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                            
+                            
+                            Text(group.location)
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                            
+                            Text("멤버 \(group.memberCounts)")
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                        }
                     }
+                    .padding(.leading, 5)
+                    
+                    Spacer()
                 }
-                .padding(.leading, 5)
-                
-                Spacer()
-            }
-            .padding()
-            .background(Color.white)
-            .cornerRadius(12)
-            .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 2)
-            .onTapGesture {
-                
+                .padding()
+                .background(Color.white)
+                .cornerRadius(12)
+                .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 2)
+                .onTapGesture {
+                    
+                }
             }
         }
     }
